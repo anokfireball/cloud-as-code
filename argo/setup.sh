28ec68635984271978474a69067ceaa9be4fcb4a
Fabian Koller <github@kthxbye.cyou>
2025-02-02 18:42:38 +0100

    🔐 SOPS secrets via KSOPS

--- argo/setup.sh
+++ argo/setup.sh
@@ -10,4 +10,6 @@ kubectl config use-context $CONTEXT
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm install argo-cd argo-cd/argo-cd --namespace $NAMESPACE --create-namespace --values $SCRIPT_DIR/system/argo-cd/values.yaml --version 7.7.22
 
+sops decrypt $SCRIPT_DIR/secrets/age-key.sops.yaml | kubectl apply -f -
+kubectl apply -f $SCRIPT_DIR/secrets.yaml
 kubectl apply -f $SCRIPT_DIR/cluster.yaml
